1fa6a273efaca9b4b413127634a3d35f01e3c5b0
Russell Wallace <russell.wallace@gmail.com>
2017-05-08 10:04:54 +0100

    main

--- sort-dimacs.js
+++ sort-dimacs.js
@@ -9,7 +9,7 @@ var fs = require('fs')
 commander.usage('[options] <files>')
 commander.version(require('./package.json').version)
 commander.parse(process.argv)
-var sizes = new Set()
+var sizes = new Map()
 var files = commandFiles.expand(commander.args, file => {
 	switch (path.extname(file)) {
 	case '.cnf':
@@ -17,6 +17,7 @@ var files = commandFiles.expand(commander.args, file => {
 	}
 })
 for (var file of files) {
+	var stats = fs.statSync(file)
 	var text = fs.readFileSync(file, 'utf8')
 	var p = dimacs.parse(text, file)
 	var atoms = new Set()
@@ -26,4 +27,17 @@ for (var file of files) {
 			var atom = polarity ? literal : literal[0]
 			atoms.add(atom)
 		}
+	sizes.set(file, [
+		atoms.size,
+		stats.size,
+	])
 }
+files.sort((a, b) => {
+	a = sizes.get(a)
+	b = sizes.get(b)
+	if (a[0] !== b[0])
+		return a[0] - b[0]
+	return a[1] - b[1]
+})
+for (var file of files)
+	console.log(file)
